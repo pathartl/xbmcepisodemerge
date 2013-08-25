b14eca08efde944e91d53ef5c4a033b668348e4e
pathartl <pat@pathartl.me>
2013-08-25 07:55:58 +0000

    Added first aired date column. Now gets show ID from GET variable 'SeriesID'. Cleaned up non-uniform quotation marks. Added comments.

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 XBMC Episode Merger
 <br><br>
 <?php
-
-	$file = 'http://thetvdb.com/data/series/75164/all/';
+	
+	// Get out show's data from TheTVDB
+	$file = 'http://thetvdb.com/data/series/'.$_GET['SeriesID'].'/all/';
 	if (!$xml = simplexml_load_file($file))
 		exit("Failed to open " / $file);
+
+	// Start outputting info from TheTVDB
 	echo "Series Name: ";
 	print_r((string) $xml->Series->SeriesName);
 	
+	// Start our massive table
 	?><table><?php
-
+	
+	// Create useful headers
 	echo '<tr id="table-header">' .
 		'<td>Episode Number</td>' .
 		'<td>Episode Name</td>' .
+		'<td>Aired Date</td>' .
 	     '</tr>';
 
+	// Grab info about each episode
 	foreach ($xml->Episode as $episode) {
 		echo '<tr id="' . $episode->id . '">';
+		// Make our episode number in the S01E01 format
 		echo '<td>S' .
-			sprintf("%02s", $episode->SeasonNumber) .
-			'E' . sprintf("%02s", $episode->EpisodeNumber) .
-			"</td>";
-		echo '<td>' . $episode->EpisodeName . "</td>";
+			sprintf('%02s', $episode->SeasonNumber) .
+			'E' . sprintf('%02s', $episode->EpisodeNumber) .
+			'</td>';
+		echo '<td>' . $episode->EpisodeName . '</td>';
+		echo '<td>' . $episode->FirstAired . '</td>';
 		echo '</tr>';
 	}
 
