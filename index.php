c9fe2e45c603ad8380e6271b5c382fed48f9b26a
pathartl <pat@pathartl.me>
2013-08-25 07:40:18 +0000

    Organized episode info into table. Changed title. Added "Episode Number" column.

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
-Cartoon Combiner for XBMC
+XBMC Episode Merger
 <br><br>
 <?php
 
-	//$var->load("http://thetvdb.com/data/series/75164/all/");
-	//echo $var;
 	$file = 'http://thetvdb.com/data/series/75164/all/';
 	if (!$xml = simplexml_load_file($file))
 		exit("Failed to open " / $file);
 	echo "Series Name: ";
 	print_r((string) $xml->Series->SeriesName);
-	foreach ($xml->Episode as $episode) {
-		echo $episode->EpisodeName . "<br>";
-	}
-	 //$a = json_decode(json_encode((array) simplexml_load_string($s)),1);
-	 //print_r($a);
+	
+	?><table><?php
 
-?>
+	echo '<tr id="table-header">' .
+		'<td>Episode Number</td>' .
+		'<td>Episode Name</td>' .
+	     '</tr>';
+
+	foreach ($xml->Episode as $episode) {
+		echo '<tr id="' . $episode->id . '">';
+		echo '<td>S' .
+			sprintf("%02s", $episode->SeasonNumber) .
+			'E' . sprintf("%02s", $episode->EpisodeNumber) .
+			"</td>";
+		echo '<td>' . $episode->EpisodeName . "</td>";
+		echo '</tr>';
+	}
+
+?></table>
